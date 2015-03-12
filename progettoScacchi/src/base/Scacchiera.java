4ec6227e7858b2a27d20e409ee9d3a52809606f1
simo <simo@simo-Notebook>
2015-03-12 09:31:37 +0100

    prova

--- progettoScacchi/src/base/Scacchiera.java
+++ progettoScacchi/src/base/Scacchiera.java
@@ -5,7 +5,7 @@ import java.awt.*;
 import javax.swing.*;
 public class Scacchiera extends JPanel {
 	private static final long serialVersionUID = 2375810646479149663L;
-	JButton BScacchiera = new JButton(new ImageIcon("C:/Users/Ave/workspace/progettoSc/immagini/scacchiera.png"));
+	JButton BScacchiera = new JButton(new ImageIcon("/home/simo/git/ProgettoSc/progettoScacchi/immagini"));
 	JPanel Gioco = new JPanel();
 	public Scacchiera() {
 		BScacchiera.setContentAreaFilled(false);
