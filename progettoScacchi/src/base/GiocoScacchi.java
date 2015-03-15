43aeaf04cf289bd2e7b80995e6d5189eb68135e5
simo <simo@simo-Notebook>
2015-03-15 17:42:28 +0100

    modifica 15/03

--- progettoScacchi/src/base/GiocoScacchi.java
+++ progettoScacchi/src/base/GiocoScacchi.java
@@ -7,8 +7,8 @@ import javax.swing.JPanel;
 
 public class GiocoScacchi extends JFrame {
 	private static final long serialVersionUID = 5453533880780430476L;
-	public GiocoScacchi(){
-		
+	
+	public GiocoScacchi(){	
 		add(new Scacchiera(), BorderLayout.WEST);
 		add(new ZonaGiocatori(), BorderLayout.CENTER);
 		pack();
